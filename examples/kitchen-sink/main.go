f1a2748dc3ee61bc79e9dc621ddd7c1f420fea40
Adam Reese <adam@reese.io>
2026-04-12 18:52:15 -0700

    Create example that tests common stdlib features
    Signed-off-by: Adam Reese <adam@reese.io>

---
+++ examples/kitchen-sink/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
+
+	spinhttp "github.com/spinframework/spin-go-sdk/v3/http"
+)
+
+func init() {
+	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintln(w, "Hello World!")
+
+		// Test for panics using the logger
+		log.Print("Hello logger")
+
+		// Reading all environment variables
+		envs := os.Environ()
+		fmt.Printf("envs: %s\n", envs)
+
+		// Reading a named environment variable
+		foo := os.Getenv("FOO")
+		fmt.Printf("FOO=%s\n", foo)
+
+		// Reading a missing file
+		_, err := os.ReadFile("nope")
+		if !errors.Is(err, fs.ErrNotExist) {
+			log.Printf("Should have path error, got: %#v\n", err)
+			return
+		}
+
+		// Reading a mounted file
+
+		dat, err := os.ReadFile("test.data")
+		if err != nil {
+			log.Print("Error calling os.ReadFile()", err)
+			return
+		}
+		if string(dat) != "This is used for testing" {
+			log.Print("Error calling os.ReadFile(). Files contents don't match")
+			log.Print("Contents: ", string(dat))
+			return
+		}
+
+		// Writing a file
+
+		// if err := os.WriteFile("test.data", []byte("some random data"), 0644); err != nil {
+		// 	log.Print("Error calling os.WriteFile(): ", err)
+		// 	return
+		// }
+
+		// Should I be able to write to a file without mounting anything to
+		// the guest?
+		// err := os.WriteFile("file.data", []byte("some random data"), 0644)
+		// if err != nil {
+		// 	log.Print("Error calling os.WriteFile()", err)
+		// 	return
+		// }
+
+	})
+}
+
+func main() {}
